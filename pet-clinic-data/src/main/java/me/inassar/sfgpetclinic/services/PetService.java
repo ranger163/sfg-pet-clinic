071bb6d63b6a538517b75716efb9d13134ab17f5
ranger163 <ahmed.nassar163@gmail.com>
2020-05-16 17:14:21 +0200

    Adding PetService interface

---
+++ pet-clinic-data/src/main/java/me/inassar/sfgpetclinic/services/PetService.java
@@ -0,0 +1,19 @@
+package me.inassar.sfgpetclinic.services;
+
+import me.inassar.sfgpetclinic.model.Pet;
+
+import java.util.Set;
+
+/**
+ * Created by Ahmed Nassar.
+ * Date: 16 May, 2020
+ * Time: 5:12 PM
+ */
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
