c0312e31139ca1e2042fc76e3004a1ba690dbfe0
ranger163 <ahmed.nassar163@gmail.com>
2020-05-16 12:45:57 +0200

    Adding PetType POJO

---
+++ src/main/java/me/inassar/sfgpetclinic/model/PetType.java
@@ -0,0 +1,19 @@
+package me.inassar.sfgpetclinic.model;
+
+/**
+ * Created by Ahmed Nassar.
+ * Date: 16 May, 2020
+ * Time: 12:37 PM
+ */
+public class PetType {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
