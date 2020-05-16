fb3eb303da46f99e02440fa0010ada1c48c5327e
ranger163 <ahmed.nassar163@gmail.com>
2020-05-16 12:45:39 +0200

    Adding Pet POJO

---
+++ src/main/java/me/inassar/sfgpetclinic/model/Pet.java
@@ -0,0 +1,39 @@
+package me.inassar.sfgpetclinic.model;
+
+import java.time.LocalDate;
+
+/**
+ * Created by Ahmed Nassar.
+ * Date: 16 May, 2020
+ * Time: 12:36 PM
+ */
+public class Pet {
+
+    private PetType petType;
+    private Owner owner;
+    private LocalDate birthDate;
+
+    public PetType getPetType() {
+        return petType;
+    }
+
+    public void setPetType(PetType petType) {
+        this.petType = petType;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+}
