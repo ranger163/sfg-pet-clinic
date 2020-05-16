03bd4abc2f2bdbd14842143fb0318a0aa9fbb308
ranger163 <ahmed.nassar163@gmail.com>
2020-05-16 17:14:54 +0200

    Adding VetService interface

---
+++ pet-clinic-data/src/main/java/me/inassar/sfgpetclinic/services/VetService.java
@@ -0,0 +1,19 @@
+package me.inassar.sfgpetclinic.services;
+
+import me.inassar.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+/**
+ * Created by Ahmed Nassar.
+ * Date: 16 May, 2020
+ * Time: 5:13 PM
+ */
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
