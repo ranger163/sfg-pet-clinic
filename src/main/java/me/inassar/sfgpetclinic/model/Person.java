56316ace56f1ed3a38cf4825f9396442723c42e7
ranger163 <ahmed.nassar163@gmail.com>
2020-05-16 12:44:20 +0200

    Adding Person POJO

---
+++ src/main/java/me/inassar/sfgpetclinic/model/Person.java
@@ -0,0 +1,28 @@
+package me.inassar.sfgpetclinic.model;
+
+/**
+ * Created by Ahmed Nassar.
+ * Date: 16 May, 2020
+ * Time: 12:34 PM
+ */
+public class Person {
+
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
