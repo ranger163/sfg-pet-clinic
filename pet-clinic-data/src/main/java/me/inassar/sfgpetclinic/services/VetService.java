d3e6bc665b2d6d971d88fe9bb464ff437f022b5b
ranger163 <ahmed.nassar163@gmail.com>
2020-05-22 14:34:22 +0200

    Refactoring VetService interface

--- pet-clinic-data/src/main/java/me/inassar/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/me/inassar/sfgpetclinic/services/VetService.java
@@ -2,19 +2,11 @@ package me.inassar.sfgpetclinic.services;
 
 import me.inassar.sfgpetclinic.model.Vet;
 
-import java.util.Set;
-
 /**
  * Created by Ahmed Nassar.
  * Date: 16 May, 2020
  * Time: 5:13 PM
  */
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 
 }
