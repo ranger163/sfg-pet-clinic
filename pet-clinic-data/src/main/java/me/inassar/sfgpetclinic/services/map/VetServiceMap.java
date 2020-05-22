f94a7e14f678d8e6e643aee9e56e6e2a90f6ac9d
ranger163 <ahmed.nassar163@gmail.com>
2020-05-22 15:09:01 +0200

    Implementing VetServiceMap class

---
+++ pet-clinic-data/src/main/java/me/inassar/sfgpetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,40 @@
+package me.inassar.sfgpetclinic.services.map;
+
+import me.inassar.sfgpetclinic.model.Vet;
+import me.inassar.sfgpetclinic.services.CrudService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+/**
+ * Created by Ahmed Nassar.
+ * Date: 22 May, 2020
+ * Time: 3:03 PM
+ */
+@Service
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Vet vet) {
+        super.delete(vet);
+    }
+
+    @Override
+    public Vet save(Vet vet) {
+        return super.save(vet.getId(), vet);
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+}
