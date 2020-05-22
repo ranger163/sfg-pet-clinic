998a983a67131d1dd5e30461a00804684ca3714f
ranger163 <ahmed.nassar163@gmail.com>
2020-05-22 16:02:48 +0200

    Adding vets index page and vets controller. Closes #7.

--- pet-clinic-web/src/main/java/me/inassar/sfgpetclinic/controllers/VetsController.java
+++ pet-clinic-web/src/main/java/me/inassar/sfgpetclinic/controllers/VetsController.java
@@ -10,8 +10,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class VetsController {
+
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
     public String listVets() {
         return "vets/index";
     }
+
 }
