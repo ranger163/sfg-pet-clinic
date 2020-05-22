1ef25b2737602b889b7a28b10fb16a731dad5c96
ranger163 <ahmed.nassar163@gmail.com>
2020-05-22 15:08:19 +0200

    Implementing OwnerServiceMap class

---
+++ pet-clinic-data/src/main/java/me/inassar/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,41 @@
+package me.inassar.sfgpetclinic.services.map;
+
+import me.inassar.sfgpetclinic.model.Owner;
+import me.inassar.sfgpetclinic.services.CrudService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+/**
+ * Created by Ahmed Nassar.
+ * Date: 22 May, 2020
+ * Time: 2:55 PM
+ */
+@Service
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(Owner owner) {
+        super.delete(owner);
+    }
+
+    @Override
+    public Owner save(Owner owner) {
+        return super.save(owner.getId(), owner);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+}
