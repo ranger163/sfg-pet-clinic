009145c763ec8e0bc3b24d3eb6bd7289f8a54b47
ranger163 <ahmed.nassar163@gmail.com>
2020-05-22 14:34:45 +0200

    Refactoring OwnerService interface

--- pet-clinic-data/src/main/java/me/inassar/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/me/inassar/sfgpetclinic/services/OwnerService.java
@@ -2,21 +2,12 @@ package me.inassar.sfgpetclinic.services;
 
 import me.inassar.sfgpetclinic.model.Owner;
 
-import java.util.Set;
-
 /**
  * Created by Ahmed Nassar.
  * Date: 16 May, 2020
  * Time: 5:09 PM
  */
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
-
-    Owner findById(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
-
 }
