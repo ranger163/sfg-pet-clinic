46092580c032a6aa786eed4a9ee0f140d88f2a89
ranger163 <ahmed.nassar163@gmail.com>
2020-05-22 14:35:05 +0200

    Refactoring PetService interface

--- pet-clinic-data/src/main/java/me/inassar/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/me/inassar/sfgpetclinic/services/PetService.java
@@ -2,19 +2,11 @@ package me.inassar.sfgpetclinic.services;
 
 import me.inassar.sfgpetclinic.model.Pet;
 
-import java.util.Set;
-
 /**
  * Created by Ahmed Nassar.
  * Date: 16 May, 2020
  * Time: 5:12 PM
  */
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 
 }
